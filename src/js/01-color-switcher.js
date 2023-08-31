6ac1995b19ff39d1ac87739cc252ea8068ead382
Zoja7 <yaozonka@gmail.com>
2023-08-31 21:59:52 +0300

    first_commit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,35 @@
+const bodyEl = document.body;
+const startBtn = document.querySelector(`[data-start]`);
+const stopBtn = document.querySelector(`[data-stop]`);
+let colorId = null;
+
+startBtn.addEventListener("click", startChangeBackground);
+stopBtn.addEventListener("click", stopChangeBackground );
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+function startChangeBackground() {
+
+    startBtn.disabled = true;
+    
+    colorId = setInterval(() => {
+           
+        const randomColorId = getRandomHexColor();
+        bodyEl.style.backgroundColor = randomColorId;
+        console.log(`Interval with id ${colorId} has started!`);
+
+        }, 1000) 
+  
+}
+
+function stopChangeBackground() {
+    
+    startBtn.disabled = false;
+    clearInterval(colorId);
+    console.log(`Interval with id ${colorId} has stopped!`);
+ }
+
+
+
